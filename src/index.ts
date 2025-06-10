e2f983d69327d375a9c399117ce6cd32401c0c80
Reginald Randolph <reginaldrandolph@Reginalds-MBP.attlocal.net>
2025-06-10 02:06:03 -0500

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,46 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { gql } from "apollo-server-core";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const typeDefs = gql`
+  type User {
+    id: String!
+    email: String!
+    name: String
+    notes: [Note!]!
+  }
+
+  type Note {
+    id: String!
+    title: String!
+    content: String!
+    user: User!
+  }
+
   type Query {
-    hello: String
+    users: [User!]!
+    notes: [Note!]!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hello from GraphQL!",
+    users: async () => await prisma.user.findMany({ include: { notes: true } }),
+    notes: async () => await prisma.note.findMany({ include: { user: true } }),
   },
 };
 
 async function startServer() {
   const app = express();
 
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
   await server.start();
   server.applyMiddleware({ app });
 
